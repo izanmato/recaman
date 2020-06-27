a3f5e240c206dc8b63f17ec87eef50750388e789
izanmato <67520846+izanmato@users.noreply.github.com>
2020-06-27 22:05:49 +0200

    Add files via upload

---
+++ recaman.py
@@ -0,0 +1,21 @@
+﻿def rec(n):
+    lst=list()
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+
+    elif rec(n-1)-n<=0:
+        return rec(n-1)+n
+
+    else:
+        for i in range(n):
+            x=rec(0+i)
+            lst.append(x)
+        if rec(n-1)-n in lst:
+            return rec(n-1)+n
+        else:
+            return rec(n-1)-n
+
+
+
